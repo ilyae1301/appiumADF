69e1f3c35d870acecd6966395855896c5c83b992
ilyae1301 <ilyep@amazon.com>
2015-10-20 16:05:54 -0400

    tenth

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -33,7 +33,7 @@ public abstract class BasePage {
     protected final AppiumDriver driver;
 
     /**
-     * Testing.  A base constructor that sets the page's driver
+     * Testing. Testing. A base constructor that sets the page's driver
      *
      * The page structure is being used within this test in order to separate the
      * page actions from the tests.
