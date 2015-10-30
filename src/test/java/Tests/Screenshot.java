93160cd1623e7343d9853d6dead7ab4b3fee332a
ilyae1301 <ilyep@amazon.com>
2015-10-30 09:37:58 -0400

    test

--- src/test/java/Tests/Screenshot.java
+++ src/test/java/Tests/Screenshot.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class Screenshot// extends TestBase
-{
-//private static RemoteWebDriver driver = null;
-                
+public class Screenshot
+{                
 public boolean takeScreenshot(final String name, RemoteWebDriver driver) 
 {
 	String screenshotDirectory = System.getProperty("appium.screenshots.dir", System.getProperty("java.io.tmpdir", ""));
@@ -16,13 +14,4 @@ public boolean takeScreenshot(final String name, RemoteWebDriver driver)
 	File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 	return screenshot.renameTo(new File(screenshotDirectory, String.format("%s.png", name)));
 }
-/*@Override
-public String getName() {
-	// TODO Auto-generated method stub
-	return null;
-}
-@Override
-public void setUpPage() {
-	// TODO Auto-generated method stub
-}*/
 }
